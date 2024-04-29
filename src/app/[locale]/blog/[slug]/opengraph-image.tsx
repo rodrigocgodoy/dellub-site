5cfe1c27193b26b1e95ad6436158443ee107ec48
Rodrigo Godoy <rodrigocgodoy@hotmail.com>
2024-04-28 23:13:09 -0300

    ✨feature: Removing font inter on og image generator

--- src/app/[locale]/blog/[slug]/opengraph-image.tsx
+++ src/app/[locale]/blog/[slug]/opengraph-image.tsx
@@ -12,9 +12,9 @@ export const contentType = 'image/png'
 // Image generation
 export default async function Image({ params: { slug} }: { params: { slug: string } }) {
   // Font
-  const interSemiBold = fetch(
-    new URL('./Inter-SemiBold.ttf', import.meta.url)
-  ).then((res) => res.arrayBuffer())
+  // const interSemiBold = fetch(
+  //   new URL('./Inter-SemiBold.ttf', import.meta.url)
+  // ).then((res) => res.arrayBuffer())
  
   return new ImageResponse(
     (
@@ -38,14 +38,14 @@ export default async function Image({ params: { slug} }: { params: { slug: strin
       // For convenience, we can re-use the exported opengraph-image
       // size config to also set the ImageResponse's width and height.
       ...size,
-      fonts: [
-        {
-          name: 'Inter',
-          data: await interSemiBold,
-          style: 'normal',
-          weight: 400,
-        },
-      ],
+      // fonts: [
+      //   {
+      //     name: 'Inter',
+      //     data: await interSemiBold,
+      //     style: 'normal',
+      //     weight: 400,
+      //   },
+      // ],
     }
   )
 }
