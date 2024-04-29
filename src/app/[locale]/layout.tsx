0497d184846c3b78a7eca9076e4f43337cc49857
Rodrigo Godoy <rodrigocgodoy@hotmail.com>
2024-04-28 23:22:54 -0300

    ✨feature: Adding vercel spped insights

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 import "./globals.css";
 import { Provider } from "./provider";
@@ -40,6 +41,7 @@ export default function RootLayout({
         <Provider locale={locale}>
           {children}
           <Analytics />
+          <SpeedInsights />
         </Provider>
       </body>
     </html>
