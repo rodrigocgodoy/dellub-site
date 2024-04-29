82340dc56de214069258ca93eebdb2ce5917035a
Rodrigo Godoy <rodrigocgodoy@hotmail.com>
2024-04-29 14:05:04 -0300

    ✨feature: Finish config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,23 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
+  darkMode: 'media',
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      fontFamily: {
+        title: ['var(--font-raleway)', 'Arial', 'sans-serif'],
+        text: ['var(--font-inter)', 'Arial', 'sans-serif'],
+      },
+      colors: {
+        brand: {
+          primary: '#6609B0',
+        },
+      },
+      container: {
+        center: true,
       },
     },
   },
